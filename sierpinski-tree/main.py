73ea11bbaf3533409ee5501b3f7a4dba864d4291
Rajdeep <rajdeep31415@gmail.com>
2024-12-27 16:03:42 +0530

    Some minor changes and the final commit before starting with snowmandelbrot, hopefully

--- sierpinski-tree/main.py
+++ sierpinski-tree/main.py
@@ -27,8 +27,8 @@ def plot_pts(pts_list, color_func = lambda pt : (255,255,255), surface = surf):
 chris_col_func = circ_gradient(width/2, height/2 - 100, ((255,50,50), (0,200,0), (255,50,50), (0,175,255)), 300)
 
 # generating the leaves and trunk
-leaves_pts = gen_tree_pts(width/2, height/2 - 115, 590, 3, 30000)
-trunk_pts = gen_carpet_pts(width/2, height/2 - 400, 175, 230, 10000)
+leaves_pts = gen_tree_pts(width/2, height/2 - 110, 590, 3, 300000)
+trunk_pts = gen_carpet_pts(width/2, height/2 - 400, 240, 240, 100000)
 
 snowflakes = []
 snowflakes_colfuncs = []
@@ -38,7 +38,7 @@ for i in range(25):
     x = width/2 + random.choice([1, -1])*(0.16*width + random.random()*0.34*width)
     y = 25 + random.random()*(height-50)
     angle = random.random()*2*pi
-    size = 25 + 50*random.random()
+    size = 15 + 65*random.random()
     initial_col = ratio_div_3d(cyan, blue, 0.5 + 0.5*random.random())
     terminal_col = ratio_div_3d(cyan, blue, 0.1 + 0.4*random.random())
     snowflakes.append(gen_vicsek_pts(x, y, size, angle, 10000))
@@ -52,7 +52,6 @@ for snowflake, col_func in zip(snowflakes, snowflakes_colfuncs):
 # plotting leaves and trunk on the surface
 plot_pts(trunk_pts, lambda pt : (110,38,14))
 plot_pts(leaves_pts, chris_col_func)
-
 
 # displaying the surface and keeping the display running
 while True:
